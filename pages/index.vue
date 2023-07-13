34c2f1ad0b2c8f96b1994aeb373b64e47785cebc
Jc Valencia <carlo.valencia066@gmail.com>
2023-07-13 12:13:22 +0800

    Sample commit to trigger gh workflow

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <br/>
       <h2 class="text-h2">I'm <span class="text-h2 text-blue-darken-4">Louie Gonzales 👋</span></h2>
       <h5 class="text-h5 mt-5 default-font" >
-        I'm a highly driven and detailed-oriented <span class="font-weight-bold primary">Software Quality Assurance Tester</span>
+        I'm a highly driven and detailed-oriented <span class="font-weight-bold">Software Quality Assurance Tester</span>
 with comprehensive experience in both manual and automated testing
 with great understanding of the software development process.
       </h5>
