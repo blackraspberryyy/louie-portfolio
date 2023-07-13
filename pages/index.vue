7483f726c517e612f26e40693e9304f6909a0bcf
Jc Valencia <carlo.valencia066@gmail.com>
2023-07-13 11:22:58 +0800

    Sample commit to trigger gh workflow

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <VContainer class="fill-height">
-    <div style="max-width: 750px">
+    <div style="max-width: 650px">
       <h3 class="text-h3">Hey there!</h3>
       <br/>
       <h2 class="text-h2">I'm <span class="text-h2 text-blue-darken-4">Louie Gonzales 👋</span></h2>
       <h5 class="text-h5 mt-5 default-font" >
-        I'm a Highly driven and detailed-oriented <span class="font-weight-bold">Software Quality Assurance Tester</span>
+        I'm a highly driven and detailed-oriented <span class="font-weight-bold">Software Quality Assurance Tester</span>
 with comprehensive experience in both manual and automated testing
 with great understanding of the software development process.
       </h5>
